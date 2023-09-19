5add46263d0f01230979a29a213969493afb8f0d
Ramakrushna Biswal <ramakrushnabunty@gmail.com>
2023-09-20 00:29:50 +0530

    connected database

--- index.js
+++ index.js
@@ -1,26 +1,7 @@
 const express = require('express');
 const path = require('path');
-const mysql = require('mysql2'); // Import the mysql2 library
 const app = express();
 const port = process.env.PORT || 3000;
-
-// Create a MySQL database connection
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'test'
-});
-
-// Connect to the MySQL database
-db.connect((err) => {
-  if (err) {
-    console.error('Error connecting to MySQL:', err);
-    return;
-  }
-  console.log('Connected to MySQL database');
-});
-
 app.use(express.static('public'));
 
 app.use('/', require(path.join(__dirname, 'routes/blog.js')));
@@ -28,7 +9,7 @@ app.use('/contact', require(path.join(__dirname, 'routes/blog.js')));
 app.use('/about', require(path.join(__dirname, 'routes/blog.js')));
 app.use('/locations', require(path.join(__dirname, 'routes/blog.js')));
 app.use('/register', require(path.join(__dirname, 'routes/blog.js')));
-
+app.use('/home', require(path.join(__dirname, 'routes/blog.js')));
 app.listen(port, () => {
   console.log(`Mr Traveller app listening on port ${port}`);
 });
--- routes/blog.js
+++ routes/blog.js
@@ -1,34 +1,67 @@
-const express = require('express')
-const path = require('path')//path function
+const express = require('express');
+const path = require('path');
 const router = express.Router();
+const mysql = require('mysql2/promise');
 
+const userdata = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'test'
+});
 
-router.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'../templates/index.html'))//use '../' because templates/index.html outside of the routes folder 
-})
-router.get('/contact',(req,res)=>{
-    res.sendFile(path.join(__dirname,'../templates/contact.html'))
-})
-router.get('/about',(req,res)=>{
-    res.sendFile(path.join(__dirname,'../templates/about.html'))
-})
-router.get('/locations',(req,res)=>{
-    res.sendFile(path.join(__dirname,'../templates/locations.html'))
-})
-router.get('/register',(req,res)=>{
-    res.sendFile(path.join(__dirname,'../templates/register.html'))
+userdata.getConnection()
+  .then((connection) => {
+    console.log('Database connected successfully!');
+    connection.release(); // Release the connection
+  })
+  .catch((err) => {
+    console.error('Error connecting to the database:', err);
+  });
 
-})
-// router.post('/', async(req, res) => {
-//     const formData = req.body;
-//     const registration = new Registration(formData)  
-//     try {
-//         const savedRegistration = await registration.save();
-//         console.log('Saved Registration:', savedRegistration);
-//         res.status(200).send('Registration submitted successfully.');
-//       } catch (error) {
-//         console.error('Error saving registration:', error);
-//         res.status(500).send('Error submitting registration.');
-//       }
-//   })
-  module.exports =  router;
+router.use(express.urlencoded({ extended: true }));
+
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../templates/index.html'));
+});
+
+router.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, '../templates/contact.html'));
+});
+
+router.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, '../templates/about.html'));
+});
+
+router.get('/locations', (req, res) => {
+  res.sendFile(path.join(__dirname, '../templates/locations.html'));
+});
+
+router.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname, '../templates/register.html'));
+});
+router.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, '../templates/index.html'));
+});
+router.post('/register', async (req, res) => {
+    const { name, email, phone, age, mygender, departuredate, returndate, destination, locations, t_and_c } = req.body;
+
+    // for debug debug
+    // console.log('Received data:', { name, email, phone, age, mygender, departuredate, returndate, destination, locations, t_and_c });
+
+    try {
+        const connection = await userdata.getConnection();
+         const destinationString = destination.join(', ');
+        const [rows, fields] = await connection.execute(
+            'INSERT INTO users (name, email, phone, age, gender, departure_date, return_date, destination, package, terms_and_conditions) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+            [name, email, phone, age, mygender, departuredate, returndate, destinationString, locations, t_and_c]
+        );        
+        connection.release(); 
+
+        res.redirect('./home');
+    } catch (error) {
+        console.error('Error inserting user data into the database:', error);
+        res.status(500).send('Registration failed. Please try again later.');
+    }
+});
+module.exports = router;
